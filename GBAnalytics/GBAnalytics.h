58de12d5831ccef84a73a89f940655f97e9e8d61
Luka Mirosevic <luka@goonbee.com>
2013-01-29 09:41:37 +0100

    Initial Commit

---
+++ GBAnalytics/GBAnalytics.h
@@ -0,0 +1,13 @@
+//
+//  GBAnalytics.h
+//  GBAnalytics
+//
+//  Created by Luka Mirosevic on 29/01/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBAnalytics : NSObject
+
+@end
